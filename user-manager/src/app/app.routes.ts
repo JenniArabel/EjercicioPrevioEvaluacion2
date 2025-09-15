dcccfdc9e7e76d6d4251c0af3a2e6f1ee42a7942
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-09-15 07:28:37 -0300

    Agregar rutas para el componente de inicio de sesión y registro utilizando Lazy Loading

--- user-manager/src/app/app.routes.ts
+++ user-manager/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    children: [
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./auth/register/register.component').then((m) => m.RegisterComponent),
+      },
+    ],
+  },
+];
